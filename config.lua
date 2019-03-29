d87c611aa01834c304d1bb883630ec9b96070d61
madhatter23412 <39780345+madhatter23412@users.noreply.github.com>
2019-03-29 16:55:45 -0400

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,50 @@
+Config              = {}
+Config.MarkerType   = 23
+Config.DrawDistance = 25.0
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
+Config.MarkerColor  = {r = 100, g = 204, b = 100}
+Config.RequiredCopsCoke = 0
+Config.RequiredCopsMeth = 0
+Config.RequiredCopsWeed = 0
+Config.RequiredCopsOpium = 0
+Config.RequiredCopsMoonshine = 0
+Config.Locale = 'en'
+
+Config.Zones = {
+	CokeFarm =			{x = -1855.79,	y = 4809.47,	    z = 2.0},
+	CokeTreatment =		{x = 1392.31,	y= 3607.99,		    z = 38.01},
+	CokeResell =		{x = 90.4615,	y = 3751.77,		z = 40.77},
+	MethFarm =			{x = 1525.29,	y = 1710.02,		z = 109.00},
+	MethTreatment =		{x = -7.77,	    y = 3715.38,		z = 38.4},
+	MethResell =		{x = -241.03,	y = 6354.96,		z = 30.6},
+	WeedFarm =			{x = 2233.6,	y = 5577.64,		z = 53.01},
+	WeedTreatment =		{x = 2329.49,	y = 2573.48,		z = 46.01},
+	WeedResell =		{x = 996.555,	y = 3574.94,		z = 34.5},
+	OpiumFarm =			{x = -1611.81,	y = 5261.25,		z = 3.04},
+	OpiumTreatment =	{x = 2492.9,	y = 3447.07,		z = 50.01},
+	OpiumResell =		{x = 263.087,	y = 2592.60,	    z = 44.01},
+	MoonshineFarm =     {x = -48.58,	y = 3100.41,	    z = 25.3},
+	MoonshineTreatment = {x = -1124.53,	y = 4892.69,	    z = 217.5},
+	MoonshineResell =   {x = 1741.95,	y = 6419.28,	    z = 34.1}
+}
+
+
+Config.Blips = {
+    {name = "Coke Farm", 		 color=74, scale=0.8, id=384,	     x = -1844.8,	y= 4817.79,	    z = 3.40},
+	{name = "Coke Treatment",  color=74, scale=0.8, id=384,          x = 1392.31,	y= 3607.99,		z = 38.01},
+	{name = "Coke Sales", 	 color=74, scale=0.8, id=384,	         x = 90.4615,	y= 3751.77,		z = 40.77},
+	{name = "Meth Farm",       color=61, scale=0.8, id=383,	         x = 1525.29,	y= 1710.02,		z = 109.00},
+	{name = "Meth Treatment",  color=61, scale=0.8, id=383,	         x = -7.77,	    y = 3715.38,    z = 38.4},
+	{name = "Meth Sales",      color=61, scale=0.8, id=383,          x = -241.03,	y = 6354.96,	z = 30.6},
+	{name = "Weed Farm",       color=25, scale=0.8, id=382,	         x = 2233.6,	y= 5577.64,		z = 53.01},
+	{name = "Weed Treatment",  color=25, scale=0.8, id=382,	         x = 2329.49,	y= 2573.48,		z = 46.01},
+	{name = "Weed Sales",	     color=25, scale=0.8, id=382,        x = 996.555,	y= 3574.94,		z = 34.5},
+	{name = "Opium Farm",      color=21, scale=0.8, id=387,	         x = -1611.81,	y = 5261.25,    z = 3.04},
+	{name = "Opium Treatment", color=21, scale=0.8, id=387,	         x = 2492.9,	y = 3447.07,    z = 50.01},
+	{name = "Opium Sales",     color=21, scale=0.8, id=387,      	 x = 263.087,	y= 2592.60,	    z = 44.01},
+	{name = "MooneShine Farm",     color=42, scale=0.8, id=381,      x = -48.4043,	y = 3093.86,	z = 26.01},
+	{name = "MooneShine Treatment",     color=42, scale=0.8, id=381, x = -1124.53,	y = 4892.69,	z = 218.01},	
+	{name = "MooneShine Sales",     color=42, scale=0.8, id=381,	 x = 1741.95,	y = 6419.28,	z = 35.039}
+
+
+}
